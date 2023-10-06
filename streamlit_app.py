91c12a8c90adcbb8b0f54ba528f18230eeb5d9f0
johnrunnner1234 <146207005+johnrunnner1234@users.noreply.github.com>
2023-10-06 14:06:19 +0100

    Update streamlit_app.py
    api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+
+
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥣 Kale, Spinach & Rocket Smoothie')
@@ -14,3 +17,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
