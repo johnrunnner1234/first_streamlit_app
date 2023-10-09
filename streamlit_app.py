c4fbf5b24a50ed1ec8b93c2a3a4d912068dc1279
johnrunnner1234 <146207005+johnrunnner1234@users.noreply.github.com>
2023-10-09 10:37:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import URLError from urllib.error
+from urllib.error import URLError
 
 
 streamlit.header('Breakfast Menu')
