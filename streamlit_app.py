ed519e10d80b18dc49ce9126be6aba3566625dbd
johnrunnner1234 <146207005+johnrunnner1234@users.noreply.github.com>
2023-10-09 14:09:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,11 +39,6 @@ except URLError as e:
 streamlit.write('The user entered ', fruit_choice)
   
 
-
-
-
-
-
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
             my_cur.execute("select * from fruit_load_list;")
